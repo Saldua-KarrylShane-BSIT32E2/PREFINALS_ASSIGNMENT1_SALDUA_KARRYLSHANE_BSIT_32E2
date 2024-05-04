3a24e49153ab33622f40102a0b23fdad4839df2d
STUDENT <Karrylshanesaldua@gmail.com>
2024-05-04 12:36:16 +0800

    install palang

---
+++ PREFINALS_AUTH_Karryl_Saldua/PREFINALS_AUTH_Karryl_Saldua/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINALS_AUTH_Karryl_Saldua.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
